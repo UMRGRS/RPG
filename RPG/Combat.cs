b39751f8690c8c58604863c001fe44798b8db030
MizFony <lizastridmarquezt@gmail.com>
2023-08-04 10:56:03 -0700

    Some notes

--- RPG/Combat.cs
+++ RPG/Combat.cs
@@ -10,9 +10,11 @@ namespace RPG
         private readonly Random ran = new Random();
         ContenedorEnemigos enemies = new ContenedorEnemigos();
         
-        public void SelectEnemies() 
+        public void SelectEnemies(int lvl) 
         {
-            
+            //Two random numbers if the number is less than the player's level then 1 more slime appers
+            int select = ran.Next(1, 12);
+
         }
     }
 }
