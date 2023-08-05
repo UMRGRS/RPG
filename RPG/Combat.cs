130bc77ef569e7edbda59e1e0b5e6fea8338ba17
UMRGRS <115056186+UMRGRS@users.noreply.github.com>
2023-08-05 12:47:34 -0700

    Enemies totally functional

--- RPG/Combat.cs
+++ RPG/Combat.cs
@@ -7,29 +7,29 @@ namespace RPG
 {
     internal class Combat
     {
-        //Necesitamos crear los objetos en tiempo de ejecucion, si lo hacemos desde el principio, si spawnean mas de un tipo a la vez se toma como un solo enemigo
+        //Hay que crear varios nombres para cada tipo de slime y diferenciarlos
         //Cada turno todos los slimes en pantalla atacaran al jugador
         private readonly Random ran = new Random(); //Clase para generar numeros random
-        private readonly ContenedorEnemigos enemies = new ContenedorEnemigos();
+
         public List<Enemigos> SpawnEnemies(int lvl) 
         {
-            //Dependiendo del nivel del jugador usamos una lista de enemigos
+            //En base al nivel del jugador hacemos aparecer slimes de nivel mas alto o bajo
             List <Enemigos> activeEnemies;
             if (lvl < 4)
             {
-                activeEnemies = SelectEnemies(enemies.EnemigosTier1, lvl);
+                activeEnemies = SelectEnemies(1, lvl);
             }
             else if (lvl > 4 && lvl < 9)
             {
-                activeEnemies = SelectEnemies(enemies.EnemigosTier2, lvl);
+                activeEnemies = SelectEnemies(2, lvl);
             }
             else 
             {
-                activeEnemies = SelectEnemies(enemies.EnemigosTier3, lvl);
+                activeEnemies = SelectEnemies(3, lvl);
             }
             return activeEnemies;
         }
-        private List<Enemigos> SelectEnemies(Enemigos[] enemigos, int lvl) 
+        private List<Enemigos> SelectEnemies(int enemyLvl, int lvl) 
         {
             //Generamos un numero aleatorio entre 0 y 12 si ese numero es menor al nivel del jugador aparece un slime extra
             //Siempre aparece un slime por defecto
@@ -47,8 +47,8 @@ namespace RPG
             //Un numero aleatorio entre 1 y 3 selecciona al slime que aparecera de la lista de enemigos disponibles
             for (int i = 0; i < enemiesQuantity; i++) 
             {
-                int index = ran.Next(0, enemigos.Length);
-                selectedEnemies.Add(enemigos[index]);
+                int index = ran.Next(0, 3);
+                selectedEnemies.Add(new Enemigos("Sparky", 1, 1, 1, index, 1, enemyLvl));
             }
             return selectedEnemies;
         }
--- RPG/ContenedorEnemigos.cs
+++ RPG/ContenedorEnemigos.cs
@@ -7,37 +7,21 @@ namespace RPG
 {
     internal class ContenedorEnemigos
     {
-        //Lugar para crear todos los enemigos que deseemos
+        //Clase en proceso de eliminacion, tal vez podemos usarla para los nombres)?
         //Fire = 1
         //Water = 2;
         //Nature = 3
-        private Enemigos[] enemigosTier1 = new Enemigos[3];
-        private Enemigos[] enemigosTier2 = new Enemigos[3];
-        private Enemigos[] enemigosTier3 = new Enemigos[3];
-        public Enemigos[] EnemigosTier1 { get => enemigosTier1; set => enemigosTier1 = value; }
-        public Enemigos[] EnemigosTier2 { get => enemigosTier2; set => enemigosTier2 = value; }
-        public Enemigos[] EnemigosTier3 { get => enemigosTier3; set => enemigosTier3 = value; }
-        public ContenedorEnemigos() 
-        {
-            CreateArrays();
-        }
-        private void CreateArrays() 
-        {
-            enemigosTier1[0] = new Enemigos("Sparks", 1, 1, 1, 1, 1, 1);
-            enemigosTier1[1] = new Enemigos("Waterfall", 1, 1, 1, 2, 1, 1);
-            enemigosTier1[2] = new Enemigos("Dendro", 1, 1, 1, 3, 1, 1);
-            //Use this to check the enemy scaling
-            /*foreach (var enemy in enemigosTier1) 
-            {
-                Console.WriteLine($"{enemy.Nombre},{enemy.Vida},{enemy.Armadura},{enemy.Dano},{enemy.Elemento},{enemy.XP_drop},{enemy.Rango}");
-            }*/
-            enemigosTier2[0] = new Enemigos("Sparks 2", 1, 1, 1, 1, 1, 2);
-            enemigosTier2[1] = new Enemigos("Waterfall 2", 1, 1, 1, 2, 1, 2);
-            enemigosTier2[2] = new Enemigos("Dendro 2", 1, 1, 1, 3, 1, 2);
 
-            enemigosTier3[0] = new Enemigos("Sparks 3", 1, 1, 1, 1, 1, 3);
-            enemigosTier3[1] = new Enemigos("Waterfall 3", 1, 1, 1, 2, 1, 3);
-            enemigosTier3[2] = new Enemigos("Dendro 3", 1, 1, 1, 3, 1, 3);
-        }
+            //enemigosTier1[0] = new Enemigos("Sparks", 1, 1, 1, 1, 1, 1);
+            //enemigosTier1[1] = new Enemigos("Waterfall", 1, 1, 1, 2, 1, 1);
+            //enemigosTier1[2] = new Enemigos("Dendro", 1, 1, 1, 3, 1, 1);
+
+            //enemigosTier2[0] = new Enemigos("Sparks 2", 1, 1, 1, 1, 1, 2);
+            //enemigosTier2[1] = new Enemigos("Waterfall 2", 1, 1, 1, 2, 1, 2);
+            //enemigosTier2[2] = new Enemigos("Dendro 2", 1, 1, 1, 3, 1, 2);
+
+            //enemigosTier3[0] = new Enemigos("Sparks 3", 1, 1, 1, 1, 1, 3);
+            //enemigosTier3[1] = new Enemigos("Waterfall 3", 1, 1, 1, 2, 1, 3);
+            //enemigosTier3[2] = new Enemigos("Dendro 3", 1, 1, 1, 3, 1, 3);
     }
 }
