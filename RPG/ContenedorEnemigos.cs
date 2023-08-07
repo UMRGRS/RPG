de68ad0837a39629dc29606d9ea7acaca956a5c7
MizFony <lizastridmarquezt@gmail.com>
2023-08-07 07:18:02 -0700

    Nombres

--- RPG/ContenedorEnemigos.cs
+++ RPG/ContenedorEnemigos.cs
@@ -12,16 +12,18 @@ namespace RPG
         //Water = 2;
         //Nature = 3
 
-            //enemigosTier1[0] = new Enemigos("Sparks", 1, 1, 1, 1, 1, 1);
-            //enemigosTier1[1] = new Enemigos("Waterfall", 1, 1, 1, 2, 1, 1);
-            //enemigosTier1[2] = new Enemigos("Dendro", 1, 1, 1, 3, 1, 1);
+        //enemigosTier1[0] = new Enemigos("Sparks", 1, 1, 1, 1, 1, 1);
+        //enemigosTier1[1] = new Enemigos("Waterfall", 1, 1, 1, 2, 1, 1);
+        //enemigosTier1[2] = new Enemigos("Dendro", 1, 1, 1, 3, 1, 1);
 
-            //enemigosTier2[0] = new Enemigos("Sparks 2", 1, 1, 1, 1, 1, 2);
-            //enemigosTier2[1] = new Enemigos("Waterfall 2", 1, 1, 1, 2, 1, 2);
-            //enemigosTier2[2] = new Enemigos("Dendro 2", 1, 1, 1, 3, 1, 2);
+        //enemigosTier2[0] = new Enemigos("Sparks 2", 1, 1, 1, 1, 1, 2);
+        //enemigosTier2[1] = new Enemigos("Waterfall 2", 1, 1, 1, 2, 1, 2);
+        //enemigosTier2[2] = new Enemigos("Dendro 2", 1, 1, 1, 3, 1, 2);
 
-            //enemigosTier3[0] = new Enemigos("Sparks 3", 1, 1, 1, 1, 1, 3);
-            //enemigosTier3[1] = new Enemigos("Waterfall 3", 1, 1, 1, 2, 1, 3);
-            //enemigosTier3[2] = new Enemigos("Dendro 3", 1, 1, 1, 3, 1, 3);
+        //enemigosTier3[0] = new Enemigos("Sparks 3", 1, 1, 1, 1, 1, 3);
+        //enemigosTier3[1] = new Enemigos("Waterfall 3", 1, 1, 1, 2, 1, 3);
+        //enemigosTier3[2] = new Enemigos("Dendro 3", 1, 1, 1, 3, 1, 3);
+
+
     }
 }
--- RPG/Items container.cs
+++ RPG/Items container.cs
@@ -11,10 +11,20 @@ namespace RPG
         //Recuerda añadir los objetos de forma cantidad,nombre.
         //Primero añade los trozos de slime en este orden, trozos de fuefo, trozos de agua, trozos de naturaleza
         //Si no entiendes algo mandame mensaje
-        Dictionary<int, string> items = new Dictionary<int, string>();
+        Dictionary<string, int> items = new Dictionary<string, int>();
         public void AddItems() 
         {
-            items.Add(1, "Trozo de slime de fuego");
+            items.Add("Athena Potion", 0); //Posion de daño
+            items.Add("Sephe Potion", 0); //Posion de vida
+            items.Add("Igna Jam", 0); //Fuego 1
+            items.Add("Hesta Jam", 0);      //2
+            items.Add("Hephe Jam", 0);      //3
+            items.Add("Natura Jam", 0); //Naturaleza 1
+            items.Add("Ninphe Jam", 0);            //2
+            items.Add("Demetra Jam", 0);           //3
+            items.Add("Aqua Jam", 0); //Agua 1
+            items.Add("Anfi Jam", 0);      //2
+            items.Add("Nepht Jam", 0);     //3
         }
     }
 }
