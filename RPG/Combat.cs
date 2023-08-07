1d358a05f73e719af0e895f34889ff9e0bf58270
MizFony <lizastridmarquezt@gmail.com>
2023-08-07 08:01:56 -0700

    Nothing to see

--- RPG/Combat.cs
+++ RPG/Combat.cs
@@ -12,6 +12,8 @@ namespace RPG
         //Cada turno todos los slimes en pantalla atacaran al jugador
         private readonly Random ran = new Random(); //Clase para generar numeros random
 
+         private readonly Utility utility = new Utility();
+
         public List<Enemigos> SpawnEnemies(int lvl) 
         {
             
@@ -49,8 +51,8 @@ namespace RPG
             //Un numero aleatorio entre 1 y 3 selecciona al slime que aparecera de la lista de enemigos disponibles
             for (int i = 0; i < enemiesQuantity; i++) 
             {
-                int enemyElement = ran.Next(1, 4);
-                selectedEnemies.Add(new Enemigos("Sparky", 1, 1, 1, enemyElement, 1, enemyLvl));
+                int enemyElement = ran.Next(0, 3);
+                selectedEnemies.Add(new Enemigos(utility.SelecNombres(enemyElement), 1, 1, 1, enemyElement, 1, enemyLvl));
             }
             return selectedEnemies;
         }
